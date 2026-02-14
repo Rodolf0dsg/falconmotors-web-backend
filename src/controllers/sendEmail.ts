855816ed502d773b9de1cfe473663129dd4dd926
Rodolfo <fororus12@gmail.com>
2026-02-14 11:07:55 -0400

    customSubject added

--- src/controllers/sendEmail.ts
+++ src/controllers/sendEmail.ts
@@ -2,11 +2,12 @@ import { makeMessage } from "../helpers/makeMessage";
 import { gmail } from "../google/gmailClient";
 
 
-export async function sendEmailAPI(name: string, email: string, message: string) {
+export async function sendEmailAPI(name: string, email: string, message: string, customSubject?:string ) {
+  const subject: string =  customSubject || `Nueva solicitud de ${name}`;
   const rawMessage = makeMessage(
     process.env.MAIL_SENDER!,
     process.env.MAIL_SENDER!,
-    `IMPORTANTE: mensaje de ${name}`,
+    subject,
     `<p><b>Nombre:</b> ${name}</p>
      <p><b>Email:</b> ${email}</p>
      <p>${message}</p>`
